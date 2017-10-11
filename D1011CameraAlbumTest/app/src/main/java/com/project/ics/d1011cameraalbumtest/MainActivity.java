c7594f4fbb80a0b3d85caf6dd47fdf9326f89645
gowufang <gowufang@gmail.com>
2017-10-11 15:02:16 +0800

    take a photo

---
+++ D1011CameraAlbumTest/app/src/main/java/com/project/ics/d1011cameraalbumtest/MainActivity.java
@@ -0,0 +1,89 @@
+package com.project.ics.d1011cameraalbumtest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int TAKE_PHOTO = 1;
+
+    private ImageView imageView;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button takePhoto= (Button) findViewById(R.id.take_photo);
+        imageView= (ImageView) findViewById(R.id.pic);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                File outputImage=new File(getExternalCacheDir(),"output_image.jpg");
+                if (outputImage.exists()){
+                    outputImage.delete();
+                }
+                try {
+                    outputImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT>=24){
+                    imageUri= FileProvider.getUriForFile(MainActivity.this,"com.example.cameraalbumtest.fileprovider",outputImage);
+                }
+                else {
+                    imageUri = Uri.fromFile(outputImage);
+                }
+
+
+
+// 启动相机程序
+                Log.d("onActivityResult2","hellooo");
+                Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                Log.d("onActivityResult3","hellooouuu");
+                startActivityForResult(intent,TAKE_PHOTO);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("onActivityResult4","hellooo onActivityResult");
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if (resultCode==RESULT_OK){
+                    try {
+                        Log.d("onActivityResult","hello");
+                        Bitmap bitmap= BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        imageView.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+
+        }
+    }
+}
