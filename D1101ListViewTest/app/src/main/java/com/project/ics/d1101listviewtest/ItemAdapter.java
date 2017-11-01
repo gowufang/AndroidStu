1dec136f86db73385319112e493d73dd98f22b50
gowufang <gowufang@gmail.com>
2017-11-01 11:34:05 +0800

    use viewholder

--- D1101ListViewTest/app/src/main/java/com/project/ics/d1101listviewtest/ItemAdapter.java
+++ D1101ListViewTest/app/src/main/java/com/project/ics/d1101listviewtest/ItemAdapter.java
@@ -33,20 +33,37 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         Log.d("Getview","getview excuted!"+position);
 //        View view= LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
         View view;
+        ViewHolder viewHolder;
         if (convertView==null){
             view=LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
 
+            viewHolder=new ViewHolder();
+            viewHolder.name= (TextView) view.findViewById(R.id.name);
+            viewHolder.age= (TextView) view.findViewById(R.id.age);
+            viewHolder.phone= (TextView) view.findViewById(R.id.phone);
+            view.setTag(viewHolder);
         }else {
             view=convertView;
+            viewHolder= (ViewHolder) view.getTag();
         }
-        TextView name=(TextView) view.findViewById(R.id.name);
+        /*TextView name=(TextView) view.findViewById(R.id.name);
         TextView age= (TextView) view.findViewById(R.id.age);
         TextView phone= (TextView) view.findViewById(R.id.phone);
 
         name.setText(item.getName());
         age.setText(item.getAge());
         phone.setText(item.getPhone());
+        原来每次都需要查找一次控件
+        */
+        viewHolder.name.setText(item.getName());
+        viewHolder.age.setText(item.getAge());
+        viewHolder.phone.setText(item.getPhone());
 
         return view;
+    }
+    class ViewHolder{
+        TextView name;
+        TextView age;
+        TextView phone;
     }
 }
